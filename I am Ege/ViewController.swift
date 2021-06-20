386fb0d66fe5b1371f4de3e596bd1b8ce75a2eb9
egesenkul <egesenkul35@hotmail.com>
2021-06-20 11:41:33 +0300

    Projeyi oluşturdum

---
+++ I am Ege/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Ege
+//
+//  Created by Ege Şenkul on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
