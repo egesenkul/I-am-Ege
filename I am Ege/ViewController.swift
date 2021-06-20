3061c6ab513ea1ce3683a370e936025002312497
egesenkul <egesenkul35@hotmail.com>
2021-06-20 14:22:52 +0300

    iOS Button Kullanımı
    iOS Uygulama Geliştirme 6 kapsamında ilk button için Action ekledim

--- I am Ege/ViewController.swift
+++ I am Ege/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showDetails(_ sender: Any) {
+        // kullanıcı butona bastığında buradaki kod bloğu çalışacaktır
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
