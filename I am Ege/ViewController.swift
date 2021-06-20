37cc1a84e1729bb889a9cfbf92e7606bb4f04034
egesenkul <egesenkul35@hotmail.com>
2021-06-20 17:09:50 +0300

    Koddan Label Yazısını Değiştirme
    iOS Uygulama Geliştirme 7 kapsamında buttona basıldıkça ekranda bulunan labellardaki yazıları güncelledim

--- I am Ege/ViewController.swift
+++ I am Ege/ViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblAgeHeader: UILabel!
+    @IBOutlet weak var lblGenderHeader: UILabel!
+    @IBOutlet weak var lblJobHeader: UILabel!
+    @IBOutlet weak var lblAge: UILabel!
+    @IBOutlet weak var lblGender: UILabel!
+    @IBOutlet weak var lblJob: UILabel!
+    @IBOutlet weak var btnShowDetails: UIButton!
     @IBAction func showDetails(_ sender: Any) {
-        // kullanıcı butona bastığında buradaki kod bloğu çalışacaktır
+        
+        // Labelların görünürlüklerini tam tersine çevirdim
+        lblAgeHeader.isHidden = !lblAgeHeader.isHidden
+        lblAge.isHidden = !lblAge.isHidden
+        lblGenderHeader.isHidden = !lblGenderHeader.isHidden
+        lblGender.isHidden = !lblGender.isHidden
+        lblJobHeader.isHidden = !lblJobHeader.isHidden
+        lblJob.isHidden = !lblJob.isHidden
+        
+        // Buttondaki yazıyı güncelleyeceğim
+        if(lblAgeHeader.isHidden){
+            btnShowDetails.setTitle("Detayları Göster", for: .normal)
+        }
+        else{
+            btnShowDetails.setTitle("Detayları Gizle", for: .normal)
+        }
+        
+        let year = Calendar.current.component(.year, from: Date()) // Mevcut yılı al
+        lblAge.text = (String)(year-1996) // Mevcut yıldan doğum yılımı çıkart ve stringe çevir
     }
     override func viewDidLoad() {
         super.viewDidLoad()
